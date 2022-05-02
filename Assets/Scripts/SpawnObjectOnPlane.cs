e8aa7fe375cd8e091d7b8065105131d0738c511e
Ilya <ilya.balashow1@gmail.com>
2022-05-02 12:36:13 +0600

    Added multiple object placement

--- Assets/Scripts/SpawnObjectOnPlane.cs
+++ Assets/Scripts/SpawnObjectOnPlane.cs
@@ -9,6 +9,11 @@ public class SpawnObjectOnPlane : MonoBehaviour
 {
     private ARRaycastManager raycastManager;
     private GameObject spawnedObject;
+    private List<GameObject> placedPrefabList = new List<GameObject>();
+
+    [SerializeField]
+    private int maxPrefabSpawnCount = 0;
+    private int placedPrefabCount;
 
     [SerializeField]
     private GameObject placeablePrefab;
@@ -22,7 +27,7 @@ public class SpawnObjectOnPlane : MonoBehaviour
 
     bool TryGetTouchPosition(out Vector2 touchPosition)
     {
-        if(Input.touchCount > 0)
+        if(Input.GetTouch(0).phase == TouchPhase.Began)
         {
             touchPosition = Input.GetTouch(0).position;
             return true;
@@ -41,16 +46,22 @@ public class SpawnObjectOnPlane : MonoBehaviour
         if(raycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
         {
             var hitPose = s_Hits[0].pose;
-
-            if(spawnedObject == null)
+            if (placedPrefabCount < maxPrefabSpawnCount)
             {
-                spawnedObject = Instantiate(placeablePrefab, hitPose.position, hitPose.rotation);
-            }
-            else
-            {
-                spawnedObject.transform.position = hitPose.position;
-                spawnedObject.transform.rotation = hitPose.rotation;
+                SpawnPrefab(hitPose);
             }
         }
+    }
+
+    public void SetPrefabType(GameObject prefabType)
+    {
+        placeablePrefab = prefabType;
+    }
+
+    private void SpawnPrefab(Pose hitPose)
+    {
+        spawnedObject = Instantiate(placeablePrefab, hitPose.position, hitPose.rotation);
+        placedPrefabList.Add(spawnedObject);
+        placedPrefabCount++;
     }
 }
